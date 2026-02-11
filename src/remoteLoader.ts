93ffdf6dd8d3a232d62ae72c4486ae0a260b8b8c
Manoa Fanantenana <manoarazafindrabe@gmail.com>
2026-02-11 08:18:17 +0300

    Fix bug plateformeRemote

--- src/remoteLoader.ts
+++ src/remoteLoader.ts
@@ -12,24 +12,14 @@ let containerPromise: Promise<RemoteContainer> | null = null;
 function loadRemoteEntry(): Promise<RemoteContainer> {
   if (containerPromise) return containerPromise;
 
-  containerPromise = new Promise<RemoteContainer>((resolve, reject) => {
-    const url = `${PLATEFORM_URL}/assets/remoteEntry.js`;
-
-    import(/* @vite-ignore */ url)
-      .then(() => {
-        const container = (window as any).plateformRemote as RemoteContainer;
-        if (!container) {
-          reject(new Error('Remote container "plateformRemote" not found on window'));
-          return;
-        }
-        // Initialize the container with empty shared scope
-        container.init({}).then(() => resolve(container)).catch(() => resolve(container));
-      })
-      .catch((err) => {
-        containerPromise = null;
-        reject(err);
-      });
-  });
+  containerPromise = import(/* @vite-ignore */ `${PLATEFORM_URL}/assets/remoteEntry.js`)
+    .then((container: RemoteContainer) => {
+      return container.init({}).then(() => container).catch(() => container);
+    })
+    .catch((err) => {
+      containerPromise = null;
+      throw err;
+    });
 
   return containerPromise;
 }
